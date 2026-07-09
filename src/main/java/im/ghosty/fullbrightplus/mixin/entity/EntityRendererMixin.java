f264e1b14b9dbc9c39f2183c014e70c61aa0b82e
Paul P. <37075355+Ghosty920@users.noreply.github.com>
2026-07-10 00:43:09 +0200

    fix: EntityRenderer keeping wrong darkened values

--- src/main/java/im/ghosty/fullbrightplus/mixin/entity/EntityRendererMixin.java
+++ src/main/java/im/ghosty/fullbrightplus/mixin/entity/EntityRendererMixin.java
@@ -14,12 +14,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(EntityRenderer.class)
 public class EntityRendererMixin {
 	
-	@Shadow
-	private float torchFlickerX;
-	@Shadow
-	private float torchFlickerDX;
-	@Shadow
-	private boolean lightmapUpdateNeeded;
 	@Unique
 	private boolean fbp$torchFlickerSet = false;
 	
@@ -33,7 +27,7 @@ public class EntityRendererMixin {
 	@Inject(method = "updateTorchFlicker", at = @At("HEAD"), cancellable = true)
 	private void fbp$updateTorchFlicker(CallbackInfo ci) {
 		if (!FBP.enabled) {
-			fbp$torchFlickerSet = true;
+			fbp$torchFlickerSet = false;
 			return;
 		}
 		if (fbp$torchFlickerSet) {
@@ -41,16 +35,12 @@ public class EntityRendererMixin {
 			return;
 		}
 		fbp$torchFlickerSet = true;
-		
-		// completely made up formula, gives stuff around what the normal function does ¯\_(:3)_/¯
-		// could probably even give like 0.1, but that wouldn't be fun (anyway it's not a big thing)
-		torchFlickerX = torchFlickerDX = (float) ((1 - Math.sqrt(Math.random() * 2)) / 2);
-		lightmapUpdateNeeded = true;
-		ci.cancel();
 	}
 	
 	/**
 	 * It goes with the idea the gamma is VERY HIGH to boost values VERY HIGH too, to be sure it's the maximum value possible each time.
+	 * <p>
+	 * Not particularly required, but could help?
 	 */
 	@Redirect(method = "updateLightmap", at = @At(value = "FIELD", target = "Lnet/minecraft/client/settings/GameSettings;gammaSetting:F", ordinal = 0, opcode = Opcodes.GETFIELD))
 	private float redirectGammaSetting(GameSettings gameSettings) {
